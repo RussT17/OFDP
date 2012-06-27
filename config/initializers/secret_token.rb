7747aa3903f80f49461a15d4827449d20a860ae8
Russell Taylor <russtaylor17@gmail.com>
2012-06-27 13:49:27 -0400

    add rails app, pre data table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ofdp::Application.config.secret_token = '37ffac0afc52a71259551699e443808197cd0dd01d306e79adc98dbdfccf40a57b00ea464350b20c3cdd85f003fdf541d9380e5596929313dd893c19ff51448b'
---
+++ db/migrate/20120627174725_add_futures_data_rows.rb
@@ -0,0 +1,7 @@
+class AddFuturesDataRows < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
