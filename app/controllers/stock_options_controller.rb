b3be053cb530676ec491243ac9a5ba8ea70c7f16
Russell Taylor <russtaylor17@gmail.com>
2012-07-12 09:53:25 -0400

    put in object view

--- app/controllers/stock_options_controller.rb
+++ app/controllers/stock_options_controller.rb
@@ -1,5 +1,8 @@
 class StockOptionsController < ApplicationController
   def show
+    the_option = StockOption.find(params[:id])
+    @rows = the_option.stock_option_data_rows
+    @title = the_option.symbol
   end
 
   def index
