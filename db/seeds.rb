7c7551c4bc72656601230626984aaae8fb749c49
Russell Taylor <russtaylor17@gmail.com>
2012-08-01 16:58:15 -0400

    doing the population in one big file instead

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require 'date'
 
 File.open('futures_file.csv','r') do |f|
-  f.each do |row|
+  f.each_with_index do |row,i|
     cells = row.split(';')
     asset = Asset.where(:exchange => cells[1], :symbol => cells[2]).first_or_create
     future = asset.futures.where(:year => cells[4].to_i, :month => cells[3]).first_or_create
     future.future_data_rows.where(:date => cells[0]).first_or_create.update_attributes(:open=>cells[5],:high=>cells[6],:low=>cells[7],:settle=>cells[8],:volume=>cells[9],:interest=>cells[10])
+    puts i
   end
 end
