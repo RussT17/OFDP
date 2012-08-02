2507453a42399828ec16a8f720331b8058820c3c
Russell Taylor <russtaylor17@gmail.com>
2012-08-02 13:08:04 -0400

    test database included, gitignore set to ignore the big csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'date'
+
 cutoff = Date.parse('2012-07-25')
 FutureDataRow.where("date <= '2012-07-25'").delete_all
 File.open(Dir[Rails.root.join "db/futures_file.csv"][0], 'r') do |f|
@@ -10,6 +11,7 @@ File.open(Dir[Rails.root.join "db/futures_file.csv"][0], 'r') do |f|
       future.future_data_rows.where(:date => cells[0]).first_or_create.update_attributes(:open=>cells[5],:high=>cells[6],:low=>cells[7],:settle=>cells[8],:volume=>cells[9],:interest=>cells[10])
     else
       asset = Asset.where(:exchange => cells[1], :symbol => cells[2]).first_or_create
+      # asset = asset_by_exchange_and_symbol["#{exchange}.#{symbol}"]
       future = asset.futures.where(:year => cells[4].to_i, :month => cells[3]).first_or_create
       future.future_data_rows.create(:date => cells[0], :open=>cells[5],:high=>cells[6],:low=>cells[7],:settle=>cells[8],:volume=>cells[9],:interest=>cells[10])
     end
