3c1a9c3100e260b5a7e87853296ae0621124147d
Russell Taylor <russtaylor17@gmail.com>
2012-08-09 17:19:08 -0400

    only valid futures are displayed, and futures are validated when they are made

--- app/controllers/futures_controller.rb
+++ app/controllers/futures_controller.rb
@@ -4,6 +4,6 @@ class FuturesController < ApplicationController
   end
   
   def index
-    @contents = Future.joins(:asset).where("assets.name is not null").order("assets.symbol,year,month").page(params[:page])
+    @contents = Future.joins(:asset).where("assets.name is not null and is_valid is true").order("assets.symbol,year,month").page(params[:page])
   end
 end
--- app/models/future.rb
+++ app/models/future.rb
@@ -1,5 +1,5 @@
 class Future < ActiveRecord::Base
-  attr_accessible :asset,:asset_id,:month,:year
+  attr_accessible :asset,:asset_id,:month,:year, :is_valid
   belongs_to :asset
   has_many :future_data_rows, :dependent => :destroy
   
@@ -7,8 +7,9 @@ class Future < ActiveRecord::Base
     Date.parse(self.year.to_s + ' ' + Ofdp::Application::MONTH_NAMES[self.month])
   end
   
-  def valid?
+  def validate
     bad_months = self.asset.invalid_contract_months.map{|row| row.month}
-    !bad_months.include? self.month
+    is_valid = (!bad_months.include? self.month)
+    self.update_attributes(is_valid: is_valid)
   end
 end
